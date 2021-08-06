4c5b6fd5a26fe396aeebe4be431ddc8860913e1f
Manish Kumar <you@example.com>
2021-08-06 13:21:50 +0530

    added error handler in epic

--- src/app/auth/state/auth.epics.ts
+++ src/app/auth/state/auth.epics.ts
@@ -1,16 +1,18 @@
-import { filter, map, mergeMap } from "rxjs/operators";
-import { AppEpic } from "../../state";
+import { of } from "rxjs";
+import { catchError, filter, map, switchMap } from "rxjs/operators";
+import { IError } from "../../shared/interfaces";
+import { appActions, AppEpic } from "../../state";
 import * as authService from "../auth.service";
 import * as authActions from "./auth.actions";
 
 const loginEpic$: AppEpic = (action$) =>
   action$.pipe(
     filter(authActions.login.match),
-    mergeMap((action) => {
-      console.log("action", action);
-      return authService
-        .login(action.payload)
-        .pipe(map((result) => authActions.loginSuccess(result)));
+    switchMap((action) => {
+      return authService.login(action.payload).pipe(
+        map((result) => authActions.loginSuccess(result)),
+        catchError((error: IError) => of(appActions.error(error)))
+      );
     })
   );
 
